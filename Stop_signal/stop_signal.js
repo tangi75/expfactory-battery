1a82f7ade909de820e27835d86d3dffe1f065e07
IanEisenberg <IanEisenberg90@gmail.com>
2015-07-06 23:15:37 -0700

    worked on local-global task

--- Stop_signal/stop_signal.js
+++ Stop_signal/stop_signal.js
@@ -109,7 +109,8 @@ for (i = 0; i< numblocks; i++) {
 /* define static blocks */
 var welcome_block = {
   type: 'text',
-  text: '<div class = centerbox><p class = block-text>Welcome to the stop signal experiment. Press any key to begin.</p></div>'
+  text: '<div class = centerbox><p class = block-text>Welcome to the stop signal experiment. Press <strong>enter</strong> to begin.</p></div>',
+  cont_key: 13,
 };
 
 var instructions_block = {
