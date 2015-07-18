0d000e5c81f3a6f5c66f8af034a03abde9a15199
IanEisenberg <IanEisenberg90@gmail.com>
2015-07-18 15:20:53 -0700

    reorganized

--- static/js/jspsych/plugins/jspsych-call-function.js
+++ static/js/jspsych/plugins/jspsych-call-function.js
@@ -1,41 +1,36 @@
-/** 
+/**
  * jspsych-call-function
  * plugin for calling an arbitrary function during a jspsych experiment
  * Josh de Leeuw
- * 
- * documentation: https://github.com/jodeleeuw/jsPsych/wiki/jspsych-call-function
- * 
-**/
+ *
+ * documentation: docs.jspsych.org
+ *
+ **/
 
 (function($) {
-    jsPsych['call-function'] = (function() {
+  jsPsych['call-function'] = (function() {
 
-        var plugin = {};
+    var plugin = {};
 
-        plugin.create = function(params) {
-            var trials = new Array(1);
-            trials[0] = {
-                "type": "call-function",
-                "func": params.func,
-                "args": params.args || [],
-                "data": (typeof params.data === 'undefined') ? {} : params.data
-            };
-            return trials;
-        };
+    plugin.create = function(params) {
+      var trials = new Array(1);
+      trials[0] = {
+        "func": params.func
+      };
+      return trials;
+    };
 
-        plugin.trial = function(display_element, block, trial, part) {
-            var return_val = trial.func.apply({}, [trial.args]);
-            if (typeof return_val !== 'undefined') {
-                block.writeData($.extend({},{
-                    trial_type: "call-function",
-                    trial_index: block.trial_idx,
-                    value: return_val
-                },trial.data));
-            }
+    plugin.trial = function(display_element, trial) {
+      var return_val = trial.func();
 
-            block.next();
-        };
+      jsPsych.data.write({
+        value: return_val
+      });
 
-        return plugin;
-    })();
+
+      jsPsych.finishTrial();
+    };
+
+    return plugin;
+  })();
 })(jQuery);
--- static/js/jspsych/plugins/jspsych-text.js
+++ static/js/jspsych/plugins/jspsych-text.js
@@ -4,8 +4,8 @@
  * This plugin displays text (including HTML formatted strings) during the experiment.
  * Use it to show instructions, provide performance feedback, etc...
  *
- * documentation: https://github.com/jodeleeuw/jsPsych/wiki/jspsych-text
- * 
+ * documentation: docs.jspsych.org
+ *
  *
  */
 
@@ -15,56 +15,46 @@
         var plugin = {};
 
         plugin.create = function(params) {
-            
-            params = jsPsych.pluginAPI.enforceArray(params, ['text','data']);
-            
+
+            params = jsPsych.pluginAPI.enforceArray(params, ['text','cont_key']);
+
             var trials = new Array(params.text.length);
             for (var i = 0; i < trials.length; i++) {
                 trials[i] = {};
-                trials[i].type = "text"; // must match plugin name
                 trials[i].text = params.text[i]; // text of all trials
                 trials[i].cont_key = params.cont_key || []; // keycode to press to advance screen, default is all keys.
-                trials[i].timing_post_trial = (typeof params.timing_post_trial === 'undefined') ? 0 : params.timing_post_trial;
-                trials[i].data = (typeof params.data === 'undefined') ? {} : params.data[i];
             }
             return trials;
         };
 
-        plugin.trial = function(display_element, block, trial, part) {
-            
+        plugin.trial = function(display_element, trial) {
+
             // if any trial variables are functions
             // this evaluates the function and replaces
             // it with the output of the function
-            trial = jsPsych.pluginAPI.normalizeTrialVariables(trial);
-            
+            trial = jsPsych.pluginAPI.evaluateFunctionParameters(trial);
+
             // set the HTML of the display target to replaced_text.
             display_element.html(trial.text);
 
             var after_response = function(info) {
-                    
+
                 display_element.html(''); // clear the display
-                
+
                 save_data(info.key, info.rt);
-                
-                if (trial.timing_post_trial > 0) {
-                    setTimeout(function() {
-                        block.next();
-                    }, trial.timing_post_trial);
-                }
-                else {
-                    block.next();
-                } // call block.next() to advance the experiment after a delay.
-                
+
+                jsPsych.finishTrial();
+
             };
 
             var mouse_listener = function(e) {
-                
+
                 var rt = (new Date()).getTime() - start_time;
-                
-                display_element.unbind('click', mouse_listener); 
-                
+
+                display_element.unbind('click', mouse_listener);
+
                 after_response({key: 'mouse', rt: rt});
-                
+
             };
 
             // check if key is 'mouse'
@@ -72,16 +62,20 @@
                 display_element.click(mouse_listener);
                 var start_time = (new Date()).getTime();
             } else {
-                jsPsych.pluginAPI.getKeyboardResponse(after_response, trial.cont_key);
+              jsPsych.pluginAPI.getKeyboardResponse({
+                callback_function: after_response,
+                valid_responses: trial.cont_key,
+                rt_method: 'date',
+                persist: false,
+                allow_held_key: false
+              });
             }
 
             function save_data(key, rt) {
-                block.writeData($.extend({}, {
-                    "trial_type": "text",
-                    "trial_index": block.trial_idx,
+                jsPsych.data.write({
                     "rt": rt,
                     "key_press": key
-                }, trial.data));
+                });
             }
         };
 
