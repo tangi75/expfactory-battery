d2560910baf37cfa022918c8203c6528c790e67a
vsoch <vsochat@stanford.edu>
2015-10-03 11:50:43 -0700

    renamed:    Analysis/load_data.py -> analysis/load_data.py 	modified:   static/js/load_experiments.js

--- static/js/load_experiments.js
+++ static/js/load_experiments.js
@@ -59,114 +59,114 @@ var experiment_names = ["IDED"]
 for (i = 0; i < experiment_names.length; i++) {
 	switch (experiment_names[i]) {
 		case "simple_rt":
-			loadjscssfile("static/css/Experiments/simple_rt.css","css")
-			loadjscssfile("static/js/Experiments/simple_rt.js","js")
+			loadjscssfile("static/css/experiments/simple_rt.css","css")
+			loadjscssfile("static/js/experiments/simple_rt.js","js")
 			break;
 		case "choice_rt":
-			loadjscssfile("static/css/Experiments/choice_rt.css","css")
-			loadjscssfile("static/js/Experiments/choice_rt.js","js")
+			loadjscssfile("static/css/experiments/choice_rt.css","css")
+			loadjscssfile("static/js/experiments/choice_rt.js","js")
 			break;
 		case "simon":
-			loadjscssfile("static/css/Experiments/simon.css","css")
-			loadjscssfile("static/js/Experiments/simon.js","js")
+			loadjscssfile("static/css/experiments/simon.css","css")
+			loadjscssfile("static/js/experiments/simon.js","js")
 			break;
 		case "ANT":
-			loadjscssfile("static/css/Experiments/ANT.css","css")
-			loadjscssfile("static/js/Experiments/ANT.js","js")
+			loadjscssfile("static/css/experiments/ANT.css","css")
+			loadjscssfile("static/js/experiments/ANT.js","js")
 			loadjscssfile("static/js/jspsych/custom_plugins/jspsych-ANT-practice.js","js")
 			break;
 		case "AX-CPT":
-			loadjscssfile("static/css/Experiments/AX-CPT.css","css")
-			loadjscssfile("static/js/Experiments/AX-CPT.js","js")
+			loadjscssfile("static/css/experiments/AX-CPT.css","css")
+			loadjscssfile("static/js/experiments/AX-CPT.js","js")
 			break;
 		case "DPX":
-			loadjscssfile("static/css/Experiments/DPX.css","css")
-			loadjscssfile("static/js/Experiments/DPX.js","js")
+			loadjscssfile("static/css/experiments/DPX.css","css")
+			loadjscssfile("static/js/experiments/DPX.js","js")
 			break;
 		case "stop_signal":
-			loadjscssfile("static/css/Experiments/stop_signal.css","css")
-			loadjscssfile("static/js/Experiments/stop_signal.js","js")
+			loadjscssfile("static/css/experiments/stop_signal.css","css")
+			loadjscssfile("static/js/experiments/stop_signal.js","js")
 			loadjscssfile("static/js/jspsych/custom_plugins/jspsych-stop-signal.js","js")
 			loadjscssfile("static/js/jspsych/plugins/jspsych-call-function.js","js")
 			break;
 		case "plus-minus":
-			loadjscssfile("static/js/Experiments/plus-minus.js","js")
+			loadjscssfile("static/js/experiments/plus-minus.js","js")
 			break;
 		case "number-letter":
-			loadjscssfile("static/css/Experiments/number-letter.css","css")
-			loadjscssfile("static/js/Experiments/number-letter.js","js")
+			loadjscssfile("static/css/experiments/number-letter.css","css")
+			loadjscssfile("static/js/experiments/number-letter.js","js")
 			break;
 		case "local-global":
-			loadjscssfile("static/css/Experiments/local-global.css","css")
-			loadjscssfile("static/js/Experiments/local-global.js","js")
+			loadjscssfile("static/css/experiments/local-global.css","css")
+			loadjscssfile("static/js/experiments/local-global.js","js")
 			break;
 		case "go-nogo":
-			loadjscssfile("static/css/Experiments/go-nogo.css","css")
-			loadjscssfile("static/js/Experiments/go-nogo.js","js")
+			loadjscssfile("static/css/experiments/go-nogo.css","css")
+			loadjscssfile("static/js/experiments/go-nogo.js","js")
 			break;
 		case "stroop":
-			loadjscssfile("static/css/Experiments/stroop.css","css")
-			loadjscssfile("static/js/Experiments/stroop.js","js")
+			loadjscssfile("static/css/experiments/stroop.css","css")
+			loadjscssfile("static/js/experiments/stroop.js","js")
 			break;
 		case "antisaccade":
-			loadjscssfile("static/css/Experiments/antisaccade.css","css")
-			loadjscssfile("static/js/Experiments/antisaccade.js","js")
+			loadjscssfile("static/css/experiments/antisaccade.css","css")
+			loadjscssfile("static/js/experiments/antisaccade.js","js")
 			break;
 		case "flanker":
-			loadjscssfile("static/css/Experiments/flanker.css","css")
-			loadjscssfile("static/js/Experiments/flanker.js","js")
+			loadjscssfile("static/css/experiments/flanker.css","css")
+			loadjscssfile("static/js/experiments/flanker.js","js")
 			break;
 		case "tone_monitoring":
-			loadjscssfile("static/js/Experiments/tone_monitoring.js","js")
+			loadjscssfile("static/js/experiments/tone_monitoring.js","js")
 			loadjscssfile("static/js/jspsych/custom_plugins/jspsych-categorize-audio.js","js")
 			loadjscssfile("static/js/jspsych/plugins/jspsych-single-audio.js","js")
 			loadjscssfile("static/js/jspsych/plugins/jspsych-call-function.js","js")
 			break;
 		case "image_monitoring":
-			loadjscssfile("static/css/Experiments/image_monitoring.css","css")
-			loadjscssfile("static/js/Experiments/image_monitoring.js","js")
+			loadjscssfile("static/css/experiments/image_monitoring.css","css")
+			loadjscssfile("static/js/experiments/image_monitoring.js","js")
 			loadjscssfile("static/js/jspsych/plugins/jspsych-call-function.js","js")
 			break;
 		case "letter_memory":
-			loadjscssfile("static/js/Experiments/letter_memory.js","js")
+			loadjscssfile("static/js/experiments/letter_memory.js","js")
 			break;
 		case "keep_track":
-			loadjscssfile("static/css/Experiments/keep_track.css","css")
-			loadjscssfile("static/js/Experiments/keep_track.js","js")
+			loadjscssfile("static/css/experiments/keep_track.css","css")
+			loadjscssfile("static/js/experiments/keep_track.js","js")
 			break;
 		case "volatile_bandit":
-			loadjscssfile("static/css/Experiments/volatile_bandit.css","css")
-			loadjscssfile("static/js/Experiments/volatile_bandit.js","js")
+			loadjscssfile("static/css/experiments/volatile_bandit.css","css")
+			loadjscssfile("static/js/experiments/volatile_bandit.js","js")
 			break;
 		case "multi-source":
-			loadjscssfile("static/css/Experiments/multi-source.css","css")
-			loadjscssfile("static/js/Experiments/multi-source.js","js")
+			loadjscssfile("static/css/experiments/multi-source.css","css")
+			loadjscssfile("static/js/experiments/multi-source.js","js")
 			break;
 		case "n-back":
-			loadjscssfile("static/js/Experiments/n-back.js","js")
+			loadjscssfile("static/js/experiments/n-back.js","js")
 			break;
 		case "adaptive-n-back":
-			loadjscssfile("static/js/Experiments/adaptive_n-back.js","js")
+			loadjscssfile("static/js/experiments/adaptive_n-back.js","js")
 			loadjscssfile("static/js/jspsych/plugins/jspsych-call-function.js","js")
 			break;
 		case "RNG":
 			loadjscssfile("static/js/jspsych/custom_plugins/jspsych-multi-button.js","js")
-			loadjscssfile("static/css/Experiments/RNG.css","css")
-			loadjscssfile("static/js/Experiments/RNG.js","js")
+			loadjscssfile("static/css/experiments/RNG.css","css")
+			loadjscssfile("static/js/experiments/RNG.js","js")
 			break;
 		case "2-stage-decision":
-			loadjscssfile("static/css/Experiments/2-stage-decision.css","css")
-			loadjscssfile("static/js/Experiments/2-stage-decision.js","js")
+			loadjscssfile("static/css/experiments/2-stage-decision.css","css")
+			loadjscssfile("static/js/experiments/2-stage-decision.js","js")
 			loadjscssfile("static/js/jspsych/plugins/jspsych-call-function.js","js")
 			break;
 		case "ART":
-			loadjscssfile("static/css/Experiments/ART.css","css")
-			loadjscssfile("static/js/Experiments/ART.js","js")
+			loadjscssfile("static/css/experiments/ART.css","css")
+			loadjscssfile("static/js/experiments/ART.js","js")
 			loadjscssfile("static/js/jspsych/plugins/jspsych-call-function.js","js")
 			break;
 		case "IDED":
-			loadjscssfile("static/js/Experiments/IDED.js","js")
-			loadjscssfile("static/css/Experiments/IDED.css","css")
+			loadjscssfile("static/js/experiments/IDED.js","js")
+			loadjscssfile("static/css/experiments/IDED.css","css")
 			loadjscssfile("static/js/jspsych/plugins/jspsych-call-function.js","js")
 			break;
 	}
