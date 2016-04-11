5689206785dc1957129d1cf7d4ffc0a23570db11
Ian <IanEisenberg90@gmail.com>
2016-04-11 14:11:31 -0700

    added a file to hold generic functions used across many podlrack experiments

---
+++ static/js/utils/poldrack_utils.js
@@ -0,0 +1,49 @@
+
+
+// Class to track focus shifts during experiment
+class focus_track {
+  constructor() {
+    this.shift_away = 0;
+  }
+
+  add_shift() {
+  	this.shift_away += 1
+  }
+
+  get_shifts() {
+    return this.shift_away;
+  }
+
+  reset() {
+  	this.shift_away = 0
+  }
+}
+
+var focuser = new focus_track()
+
+$(window).blur(function(){
+  focuser.add_shift()
+});
+
+/**
+* Adds the experiment ID as well as the number of focus shifts and whether the experiment was in
+* full screen during that trial
+* @param {exp_id} string to specify the experiment id
+*/
+function addID(exp_id) {
+	var full_screen_on = window.innerHeight == screen.height
+	jsPsych.data.addDataToLastTrial({
+		exp_id: name,
+		full_screen: full_screen_on,
+		focus_shifts: focuser.get_shifts()
+	})
+	focuser.reset()
+}
+
+/*
+* Adds a display stage rather than the generic jsPsych background element
+*/
+function getDisplayElement() {
+  $('<div class = display_stage_background></div>').appendTo('body')
+  return $('<div class = display_stage></div>').appendTo('body')
+}
